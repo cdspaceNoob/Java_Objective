9d29f3503e408a8b346c5407406b648b2117fcab
voyager <cdspacenoob@gmail.com>
2022-09-09 17:49:17 +0900

    annotation ok

--- Fm/src/main/java/fm/pack/servlet/FmController.java
+++ Fm/src/main/java/fm/pack/servlet/FmController.java
@@ -13,27 +13,33 @@ import javax.servlet.http.HttpServletResponse;
 public class FmController extends HttpServlet{
 	
 	public void doHandler(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.setCharacterEncoding("utf-8");	//request 객체에 담겨오는 한글 보정 
-		resp.setContentType("text/html; charset=utf-8"); //response 객체에 들어가는 한글 보정 
-		String url = req.getRequestURL().toString();
-		String context = req.getContextPath();
-		int len = context.length();
-		String uri = req.getRequestURI();
-		String[] temp = uri.split("/");
-		String command = temp[temp.length-1];
-//		String uri2 = uri.substring(len+1);
+		req.setCharacterEncoding("utf-8");					//request 객체에 담겨오는 한글 보정 
+		resp.setContentType("text/html; charset=utf-8"); 	//response 객체에 들어가는 한글 보정 
+		String url = req.getRequestURL().toString();		// url 확인
+		String context = req.getContextPath(); 				// context 경로 확인
+		String uri = req.getRequestURI();					// uri 확인
+		String[] temp = uri.split("/");						// uri를 분리 
+		String command = temp[temp.length-1]; 				// uri의 마지막 요소 확인
 		
 		PrintWriter out = resp.getWriter();
-		out.print("<h1>hi servlet!</h1>");
-		out.print("<h1>uri ="+uri+"</h1>");
-//		out.print("<h1>uri2 ="+uri2+"</h1>");
-		out.print("<h1>url ="+url+"</h1>");
-		out.print("<h1>context path ="+context+"</h1>");
-		out.print("<h1>command ="+command+"</h1>");
+//		out.print("<h1>hi servlet!</h1>");
+//		out.print("<h1>uri ="+uri+"</h1>");
+//		out.print("<h1>url ="+url+"</h1>");
+//		out.print("<h1>context path ="+context+"</h1>");
+//		out.print("<h1>command ="+command+"</h1>");
 		
-		if(command.equals("list.do")) {
-			out.print("<h1>command ="+command+"</h1>");
-			out.print("<h1>list.do로 접속한 결과입니다</h1>");
+		if(command.equals("read.do")) {
+			out.print("<h1>read.do로 접속한 결과입니다</h1>");
+			out.print("<h1>command= "+command+"</h1>");
+		}else if(command.equals("create.do")) {
+			out.print("<h1>create.do로 접속한 결과입니다</h1>");
+			out.print("<h1>command= "+command+"</h1>");
+		}else if(command.equals("update.do")) {
+			out.print("<h1>update.do로 접속한 결과입니다</h1>");
+			out.print("<h1>command= "+command+"</h1>");
+		}else if(command.equals("delete.do")) {
+			out.print("<h1>delete.do로 접속한 결과입니다</h1>");
+			out.print("<h1>command= "+command+"</h1>");
 		}
 	}
 	
